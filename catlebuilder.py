c14524943ea3cc8213876c3f007eb2ac419618ea
IsaWad <isak.wadelius@skola.taby.se>
2023-11-29 10:53:23 +0100

    1

---
+++ catlebuilder.py
@@ -0,0 +1,29 @@
+class Castle:
+    def __init__(self):
+        self.mainCeepLVL = 1
+        self.innerWallLVL = 0
+        self.innerWestTowerLVL = 0
+        self.innerEastTowerLVL = 0
+        self.outerWallLVL = 0
+        self.outerWestTowerLVL = 0
+        self.outerEastTowerLVL = 0
+        self.gateLVL = 0
+        self.farmLVL = 1
+        self.moat = 0
+        self.weather = 0 #0 = rain, 1 cloud, 2 some sun, 3, sunny, 4 = rainbow
+        self.region = "" #Europa or Japan
+        #self.barack = 0
+
+    def tax():
+
+    def upgradingPrice(buildingLVL):
+    
+    def upgradeBuilding(building, buidlingLVL, buidlingPrice):
+        
+    def save():
+        
+    def load():
+
+    def setWeather(self, weather):
+        #Set ewther based on castle level?
+
