dee18a933cdb86bb0809fa98fd49657eb86c220a
IsaWad <isak.wadelius@skola.taby.se>
2023-12-11 11:16:19 +0100

    11/12 1

--- catlebuilder.py
+++ catlebuilder.py
@@ -1,6 +1,8 @@
 class Castle:
     def __init__(self):
-        self.mainCeepLVL = 1
+        self.estetics = ""
+        self.treasury = 500
+        self.mainKeepLVL = 1
         self.innerWallLVL = 0
         self.innerWestTowerLVL = 0
         self.innerEastTowerLVL = 0
@@ -9,21 +11,72 @@ class Castle:
         self.outerEastTowerLVL = 0
         self.gateLVL = 0
         self.farmLVL = 1
-        self.moat = 0
-        self.weather = 0 #0 = rain, 1 cloud, 2 some sun, 3, sunny, 4 = rainbow
+        self.weather = 1 #0 = rain, 1 cloud, 2 some sun, 3, sunny, 4 = rainbow
         self.region = "" #Europa or Japan
-        #self.barack = 0
 
-    def tax():
+    def tax(self):
+        farm_tax = self.farmLVL * self.weather  # Use wether as a multiplier for farmLVL
+        excluded_attributes = ['weather', 'farmLVL']  # Attributes to exclude
+        total_tax = 0
 
-    def upgradingPrice(buildingLVL):
+        for attr, value in self.__dict__.items():
+            if attr not in excluded_attributes and isinstance(value, (int, float)):
+                total_tax += value
+
+        return total_tax + farm_tax
+        
+    def upgrading_price(self, building_lvl):
+        base_cost = 100  # Cost of the first upgrade
+        upgrade_factor = 1.5  # Adjust this factor to suit the cost increase rate
+
+        # Calculate the cost based on the building level
+        upgrade_cost = base_cost * (upgrade_factor ** (building_lvl - 1))
+        return upgrade_cost
     
-    def upgradeBuilding(building, buidlingLVL, buidlingPrice):
-        
-    def save():
-        
-    def load():
+    def upgrade_building(self, building_name):
+        # Check if the building exists in the castle
+        if hasattr(self, building_name + 'LVL'):
+            current_level = getattr(self, building_name + 'LVL')
+            upgrade_cost = self.upgrading_price(current_level + 1)
 
-    def setWeather(self, weather):
+            # Check if the castle has enough resources to perform the upgrade
+            if upgrade_cost <= self.treasury:
+                # Deduct the upgrade cost and perform the upgrade
+                self.treasury -= upgrade_cost
+                setattr(self, building_name + 'LVL', current_level + 1)
+                print(f"{building_name} upgraded to level {current_level + 1}")
+            else:
+                print("Insufficient coins to upgrade")
+
+        else:
+            print("Building not found")
+
+    #def save():
+        
+    #def load():
+
+    #def setWeather(self, weather):
         #Set ewther based on castle level?
+
+    #def wind(self, region):
+
+class EuropeanCastle(Castle):
+    def __init__(self):
+        super().__init__()
+        self.region = "Europe"
+        self.armoryLVL = 0  # Additional attribute specific to European castles
+        self.moat = 0
+
+
+class JapaneseCastle(Castle):
+    def __init__(self):
+        super().__init__()
+        self.region = "Japan"
+        self.dojoLVL = 1  # Additional attribute specific to Japanese castles
+    
+
+
+japan = JapaneseCastle()
+
+#japan.upgrade_building("mainKeep")
 
